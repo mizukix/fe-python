0d136f4db0cc2d72a3cba16291020f6b1f3ec8a3
Mizuki Seto <mizuki@wakuwakustudyworld.co.jp>
2021-04-19 11:35:40 +0900

    第2版のソースコードを追加

---
+++ main.c
@@ -0,0 +1,5 @@
+int main() {
+   /* for testing dump() */
+   dump("main.c", 0L, -1L);
+   dump("main.c", 17L, 40L);
+}
---
+++ main100.c
@@ -0,0 +1,5 @@
+int main(){
+   /* for testing dump() */
+   dump("main.c", 0, -1);
+   dump("main.c", 17, 40);
+}
